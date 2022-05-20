4d7e1293aaa103b08e63dd26a2d790490a1d51ce
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-21 01:16:05 +0530

    Added : rules page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import {createRoot} from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { makeServer } from "./server";
+import { BrowserRouter } from 'react-router-dom';
+import { QuizProvider } from "./context/quiz-context";
 const root = createRoot(document.getElementById("root"));
 // Call make Server
 makeServer();
 
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <QuizProvider>
     <App />
+    </QuizProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
