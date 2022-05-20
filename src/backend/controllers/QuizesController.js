bc67b564a0e879dc09d6bef202ae2b522c542cf7
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-20 23:22:10 +0530

    fix: category spelling

--- src/backend/controllers/QuizesController.js
+++ src/backend/controllers/QuizesController.js
@@ -118,7 +118,7 @@ export const addQuizHandler = function (schema, request) {
       _id: uuid(),
       createdAt: formatDate(),
       updatedAt: formatDate(),
-      catergoryName: "",
+      categoryName: "",
       ...quiz,
     };
     const createdQuiz = schema.quizzes.create(newQuiz);
