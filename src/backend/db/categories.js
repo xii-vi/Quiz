89cea88ec8f3f267808282f3360222e12090ab9d
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-20 23:22:59 +0530

    added : Quiz database

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,14 +1,100 @@
 import { v4 as uuid } from "uuid";
 
 /**
- * Category Database can be added here.
- * You can add category of your wish with different attributes
+ * categoryName Database can be added here.
+ * You can add categoryName of your wish with different attributes
  * */
 
 export const categories = [
-  {
+{ 
+    categoryName: "Books", 
     _id: uuid(),
-    categoryName: "TV Show",
-    description: "Quizes based on TV shows",
-  },
+    description : 'Quizes based on books' 
+},
+{ 
+    categoryName: "Films", 
+    _id: uuid(),
+    description : 'Quizes based on films'  
+},
+{ 
+    categoryName: "Music", 
+    _id: uuid(),
+    description : 'Quizes based on music'  
+},
+{ 
+    categoryName: "Television", 
+    _id: uuid(),
+    description : 'Quizes based on television'  
+},
+{ 
+    categoryName: "Video Games", 
+    _id: uuid(),
+    description : 'Quizes based on Video games'  
+},
+
+{ 
+    categoryName: "Science and Nature", 
+    _id: uuid(),
+    description : 'Quizes based on nature-and-science'  
+},
+{ 
+    categoryName: "Computer", 
+    _id: uuid(),
+    description : 'Quizes based on computers'  
+},
+{ 
+    categoryName: "Mathematics", 
+    _id: uuid(),
+    description : 'Quizes based on maths'  
+},
+{ 
+    categoryName: "Mythology", 
+    _id: uuid(),
+    description : 'Quizes based on Mythology'  
+},
+{ 
+    categoryName: "Sports", 
+    _id: uuid(), 
+    description : 'Quizes based on sports' 
+},
+{ 
+    categoryName: "Geography", 
+    _id: uuid(),
+    description : 'Quizes based on Geography' 
+},
+{ 
+    categoryName: "History", 
+    _id: uuid(),
+    description : 'Quizes based on history' 
+},
+{ 
+    categoryName: "Art", 
+    _id: uuid(),
+    description : 'Quizes based on art' 
+},
+{ 
+    categoryName: "Vehicles", 
+    _id: uuid(),
+    description : 'Quizes based on vehicles' 
+},
+{ 
+    categoryName: "Comics",
+    _id: uuid(),
+    description : 'Quizes based on comics'
+    },
+{ 
+    categoryName: "Gadgets", 
+    _id: uuid(),
+    description : 'Quizes based on Gadgets' 
+},
+{ 
+    categoryName: "Japanese Anime", 
+    _id: uuid(),
+    description : 'Quizes based on anime'
+},
+{ 
+    categoryName: "Cartoon and Animations", 
+    _id: uuid(),
+    description : 'Quizes based on cartoon' 
+},
 ];
