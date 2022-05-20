e312d6b6d02b175a0aaf5f58faeae94c6e0714d2
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-20 23:23:28 +0530

    config : tailwindCSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import { makeServer } from "./server";
 const root = createRoot(document.getElementById("root"));
 // Call make Server
