170cdc7bf40f0127dfb7f9cd133399bab05f3c14
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-21 01:17:11 +0530

    update: categories data

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -9,92 +9,110 @@ export const categories = [
 { 
     categoryName: "Books", 
     _id: uuid(),
-    description : 'Quizes based on books' 
+    description : 'Quizes based on books',
+    value: 10,
 },
 { 
     categoryName: "Films", 
     _id: uuid(),
-    description : 'Quizes based on films'  
+    description : 'Quizes based on films',
+    value: 11,
 },
 { 
     categoryName: "Music", 
     _id: uuid(),
-    description : 'Quizes based on music'  
+    description : 'Quizes based on music',
+    value: 12 
 },
 { 
     categoryName: "Television", 
     _id: uuid(),
-    description : 'Quizes based on television'  
+    description : 'Quizes based on television',
+    value: 14,
 },
 { 
     categoryName: "Video Games", 
     _id: uuid(),
-    description : 'Quizes based on Video games'  
+    description : 'Quizes based on Video games',
+    value: 15,
 },
 
 { 
     categoryName: "Science and Nature", 
     _id: uuid(),
-    description : 'Quizes based on nature-and-science'  
+    description : 'Quizes based on nature-and-science',
+    value: 17,
 },
 { 
     categoryName: "Computer", 
     _id: uuid(),
-    description : 'Quizes based on computers'  
+    description : 'Quizes based on computers',
+    value: 18,
 },
 { 
     categoryName: "Mathematics", 
     _id: uuid(),
-    description : 'Quizes based on maths'  
+    description : 'Quizes based on maths',
+    value: 19,
 },
 { 
     categoryName: "Mythology", 
     _id: uuid(),
-    description : 'Quizes based on Mythology'  
+    description : 'Quizes based on Mythology',
+    value: 20,
 },
 { 
     categoryName: "Sports", 
     _id: uuid(), 
-    description : 'Quizes based on sports' 
+    description : 'Quizes based on sports',
+    value: 21,
 },
 { 
     categoryName: "Geography", 
     _id: uuid(),
-    description : 'Quizes based on Geography' 
+    description : 'Quizes based on Geography',
+    value: 22,
 },
 { 
     categoryName: "History", 
     _id: uuid(),
-    description : 'Quizes based on history' 
+    description : 'Quizes based on history',
+    value: 23, 
 },
 { 
     categoryName: "Art", 
     _id: uuid(),
-    description : 'Quizes based on art' 
+    description : 'Quizes based on art',
+    value: 25,
 },
 { 
     categoryName: "Vehicles", 
     _id: uuid(),
-    description : 'Quizes based on vehicles' 
+    description : 'Quizes based on vehicles',
+    value: 28,
 },
 { 
     categoryName: "Comics",
     _id: uuid(),
-    description : 'Quizes based on comics'
+    description : 'Quizes based on comics',
+    value: 29,
     },
 { 
     categoryName: "Gadgets", 
     _id: uuid(),
-    description : 'Quizes based on Gadgets' 
+    description : 'Quizes based on Gadgets',
+    value: 30,
 },
 { 
     categoryName: "Japanese Anime", 
     _id: uuid(),
-    description : 'Quizes based on anime'
+    description : 'Quizes based on anime',
+    value: 31,
 },
 { 
     categoryName: "Cartoon and Animations", 
     _id: uuid(),
-    description : 'Quizes based on cartoon' 
+    description : 'Quizes based on cartoon',
+    value: 32,
 },
 ];
