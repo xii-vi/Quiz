789e5e25d16c352de7100c6b49576bcad283d566
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-05-21 15:22:24 +0530

    added: Scroll to top function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./index.css"
 import { Homepage,QuizQuestion,RulesPage,Login,Signup,RequireAuth,ResultPage, ErrorPage } from "./pages";
 import {Routes,Route} from "react-router-dom"
 import { useTheme } from "./context/themeContext";
+import { ScrollToTop } from "./components/scrollToTop";
 function App() {
   const {theme} = useTheme();
   return (
     <div className= {theme === "light" ? "bg-white" : "bg-slate-900 text-slate-50"}>
+      <ScrollToTop />
       <Navbar />
       <Routes>
       <Route path="/" element={<Homepage />} />
